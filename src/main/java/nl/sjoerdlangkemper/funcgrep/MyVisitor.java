db5ba75628f28d0bf76b5c369144e0f61972f73f
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2018-01-24 17:11:59 +0100

    Cleanup visitor.
    Doesn't work yet

--- src/main/java/nl/sjoerdlangkemper/funcgrep/MyVisitor.java
+++ src/main/java/nl/sjoerdlangkemper/funcgrep/MyVisitor.java
@@ -6,52 +6,38 @@ import org.antlr.v4.runtime.tree.ParseTree;
 public class MyVisitor extends CSharpParserBaseVisitor<Nodes> {
 	@Override public Nodes visitClass_definition(CSharpParser.Class_definitionContext ctx) { 
         String className = ctx.identifier().getText();
-        System.out.println(className);
-        return visitChildren(ctx); 
+        ClassNode classNode = new ClassNode(className);
+        Nodes childNodes = visitChildren(ctx); 
+        System.out.println("Class children: " + childNodes);
+        return classNode.toNodes();
     }
 
     // Class member declaration. This thing holds both the attributes and the method declaration.
 	@Override public Nodes visitClass_member_declaration(CSharpParser.Class_member_declarationContext ctx) { 
+        Nodes declaration = visit(ctx.common_member_declaration());
     
         ParseTree attrs = ctx.attributes();
         if (attrs!=null) {
-            Object res= visit(ctx.attributes());
-            System.out.println("Res: " + res);
-        }
-        return visitChildren(ctx);
-
-
-/*
-        Object declaration = visit(ctx.common_member_declaration());
-        if (declaration instanceof MethodNode) {
-            System.out.println("Found method: "+declaration);
+            Nodes attributeNodes = visit(ctx.attributes());
+            System.out.println("Res: " + attributeNodes);
         }
 
-        ParseTree attributes = ctx.attributes();
-        if (attributes != null) {
-            System.out.println("===============");
-            Object result = visit(attributes);
-            System.out.println("Res: " + result);
-            System.out.println("^^^^^^^^^^^^^^^");
-        }
+        System.out.println("decl: " + declaration);
         return declaration;
-        */
     }
 
 	@Override public Nodes visitAttribute(CSharpParser.AttributeContext ctx) { 
         String attrName = ctx.namespace_or_type_name().identifier().get(0).getText();
-        System.out.println("Attr: " + attrName);
         return new AttributeNode(attrName).toNodes();
     }
 
 	@Override public Nodes visitMethod_declaration(CSharpParser.Method_declarationContext ctx) { 
         String methodName = ctx.method_member_name().identifier().get(0).getText();
-        System.out.println("method: "+methodName);
-        MethodNode method = new MethodNode(methodName);
-        return method.toNodes();
+        return new MethodNode(methodName).toNodes();
     }
 
     @Override protected Nodes aggregateResult(Nodes aggregate, Nodes nextResult) {
+        System.out.println(aggregate + " + " + nextResult);
         if (aggregate == null) {
             return nextResult;
         }
